4f7b15bbdf39419686545948723f4972acbada20
tkrofecheck <tkrofecheck@users.noreply.github.com>
2023-06-13 10:48:09 -0400

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,33 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+
+// https://vitejs.dev/config/
+
+const config = {
+  poc: {
+    entry: resolve('./src/main.poc.ts'),
+    fileName: 'lrs-poc.js',
+  },
+  prod: {
+    entry: resolve('./src/main.prod.ts'),
+    fileName: 'lrs.js',
+  },
+};
+
+const currentConfig = config[process.env.LIB_NAME];
+
+if (currentConfig === undefined) {
+  throw new Error('LIB_NAME is not defined or is not valid');
+}
+
+export default defineConfig({
+  plugins: [svelte()],
+  build: {
+    cssCodeSplit: false,
+    lib: {
+      ...currentConfig,
+      formats: [],
+    },
+  },
+});
